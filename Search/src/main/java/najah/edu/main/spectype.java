b7a9985e21602298403777fcc0fcd17462a5a8be
wafeqa <wafeqa@DESKTOP-DQE9296>
2020-11-19 23:22:28 -0800

    my refactor code

---
+++ Search/src/main/java/najah/edu/main/spectype.java
@@ -0,0 +1,16 @@
+package najah.edu.main;
+
+public class spectype {
+
+	private String type;
+
+	public spectype(String type) {
+		this.type = type;
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getType() {
+		return type;
+	}
+
+}
